3be2f205393e657ae6e4a7c50463cf6c08197165
Mottais <mottais.afy@outlook.fr>
2024-03-04 15:02:52 +0100

    Test Checker

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python3
 """definition of class used to store objects"""
 import json
-import importlib
 
 
 class FileStorage:
-    """
-    This class represents a file storage system
-    for objects in the application.
-    """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Returns a dictionary '__objects' containing all objects."""
+        """Returns a dictionaty '__objects'"""
         return self.__objects
 
     def new(self, obj):
-        """Adds the object saved to '__objects' dictionary."""
-        index = obj.__class__.__name__ + "." + obj.id
+        """Sets in '__objects' the object with the key """
+        index = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[index] = obj
 
     def save(self):
-        """Serializes '__objects' dictionary to a JSON file."""
+        """Serializes '__objects' dictionary to a JSON file"""
         dict_to_save = {}
         for key, obj in self.__objects.items():
             dict_to_save[key] = obj.to_dict()
@@ -30,12 +25,17 @@ class FileStorage:
             json.dump(dict_to_save, file)
 
     def reload(self):
-        """Deserializes the JSON file and loads objects into '__objects'."""
+        """Deserializes the JSON file to __objects' dictionary'"""
         from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.review import Review
+        from models.amenity import Amenity
+        from models.place import Place
         try:
             with open(self.__file_path, "r") as file:
-                Dico_objets = json.load(file)
-                for key, value in Dico_objets.items():
-                    self.__objects[key] = BaseModel(**value)
+                for key, value in json.load(file).items():
+                    self.__objects[key] = eval(value["__class__"])(**value)
         except Exception:
             pass
