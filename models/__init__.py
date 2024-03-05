0df30b47b68ff464ea382f4188a6a96b661f2ef0
Mottais <mottais.afy@outlook.fr>
2024-03-05 14:40:21 +0100

    Task9

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,11 @@
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.review import Review
+from models.amenity import Amenity
+from models.place import Place
 
 storage = FileStorage()
 storage.reload()
